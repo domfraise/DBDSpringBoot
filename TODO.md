929f2fde2eff0939a86d8756b2b70f219f9b44fc
Dom Fraise <domfraise@gmail.com>
2018-06-24 09:29:02 +0000

    Start to add login page

---
+++ TODO.md
@@ -0,0 +1,6 @@
+TODO
+
+* Create log-in button
+* Create log-in form
+* Create routing to login-page
+* Create DB for log in info
--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,21 +1,25 @@
 package src.main.java.hello;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
-
+import model.*;
 @Controller
 public class HelloController {
-    public String name = "defuault";
-	
+
 	@RequestMapping("/index2")
-    public String index2() {
-        return "index2";
-    }  
-    
-//    @RequestMapping("/name/{name}")
-//    public String name(@PathVariable String input){
-//        this.name = input;
-//	return "index3";
- //   }  
+	public String index2() {
+		return "index2";
+	}  
+
+	@RequestMapping(value="/index2", method=RequestMethod.POST)
+	public String index2(@ModelAttribute("user") User user) {
+		System.out.println(user.name);
+		return "index2";
+	}  
+
+	//    @RequestMapping("/name/{name}")
+	//    public String name(@PathVariable String input){
+	//        this.name = input;
+	//	return "index3";
+	//   }  
 }
