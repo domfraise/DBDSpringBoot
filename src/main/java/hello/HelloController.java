10362c7200caa1443b207ee5777486c7be8be0e9
Dominic Fraise <domfraise@gmail.com>
2018-06-24 11:10:29 +0100

    Get user input from UI into back end from log in form.

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,18 +1,22 @@
 package src.main.java.hello;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.*;
+
+import model.User;
 import org.springframework.stereotype.Controller;
-import model.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HelloController {
 
 	@RequestMapping("/index2")
-	public String index2() {
+	public String index2(Model model) {
+		model.addAttribute("user", new User());
 		return "index2";
 	}  
 
 	@RequestMapping(value="/index2", method=RequestMethod.POST)
-	public String index2(@ModelAttribute("user") User user) {
+	public String index2(@ModelAttribute(value = "user") User user) {
 		System.out.println(user.name);
 		return "index2";
 	}  
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,9 +1,14 @@
 package model;
 
-@ModelAttribute(value = "user")
 public class User{
 	public String name;
-	public User(String name){
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
 		this.name = name;
 	}
+
 }
