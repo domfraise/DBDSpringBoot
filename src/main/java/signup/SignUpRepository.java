5c87307221d9d4637d97eae561c7632a69cec062
FraiseD <dominic.fraise@thehutgroup.com>
2018-11-09 13:28:01 +0000

    Merge branch 'master' of C:\Users\FraiseD\Documents\Docs\Code\DBDSpringBoot with conflicts.

--- src/main/java/signup/SignUpRepository.java
+++ src/main/java/signup/SignUpRepository.java
@@ -5,17 +5,17 @@ import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-@Repository
+//@Repository
 public class SignUpRepository {
-
-    private JdbcTemplateAutoConfiguration jdbcTemplateAutoConfiguration;
-    private JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    public SignUpRepository(JdbcTemplateAutoConfiguration jdbcTemplateAutoConfiguration) {
-        this.jdbcTemplateAutoConfiguration = jdbcTemplateAutoConfiguration;
-        jdbcTemplate = jdbcTemplateAutoConfiguration.jdbcTemplate();
-    }
-
+//
+//    private JdbcTemplateAutoConfiguration jdbcTemplateAutoConfiguration;
+//    private JdbcTemplate jdbcTemplate;
+//
+//    @Autowired
+//    public SignUpRepository(JdbcTemplateAutoConfiguration jdbcTemplateAutoConfiguration) {
+//        this.jdbcTemplateAutoConfiguration = jdbcTemplateAutoConfiguration;
+//        jdbcTemplate = jdbcTemplateAutoConfiguration.jdbcTemplate();
+//    }
+//
 
 }
