39cd6442ca61410fa6e9684685a2ace510552920
FraiseD <dominic.fraise@thehutgroup.com>
2018-12-15 20:55:20 +0000

    Add skills icons

--- frontend/src/components/Languages.vue
+++ frontend/src/components/Languages.vue
@@ -5,31 +5,37 @@
         <h2 class="is-2 title">Languages & Technologies</h2>
 
         <div class="tile is-ancestor">
+
           <div class="tile is-vertical">
+
             <div class="tile">
               <div class="tile is-parent">
                 <div class="tile is-child box">
                   <p class="title">Java</p>
+                  <i class="fab fa-java fa-4x"></i>
                 </div>
               </div>
               <div class="tile is-parent">
                 <div class="tile is-child box">
                   <p class="title">Python</p>
+                  <i class="fab fa-python fa-4x"></i>
                 </div>
               </div>
-
             </div>
+
             <div class="tile">
               <div class="tile is-vertical">
                 <div class="tile">
                   <div class="tile is-parent">
                     <div class="tile is-child box">
                       <p class="title">Spring</p>
+                      <img src="../assets/icons/spring.svg" width="60"/>
                     </div>
                   </div>
                   <div class="tile is-parent">
                     <div class="tile is-child box">
                       <p class="title">Guice</p>
+                      <img src="../assets/icons/guice.png" width="60"/>
                     </div>
                   </div>
                 </div>
@@ -38,53 +44,78 @@
                   <div class="tile is-parent ">
                     <div class="tile is-child box">
                       <p class="title">Maven</p>
+                      <img src="../assets/icons/maven.png" width="60"/>
+
                     </div>
                   </div>
                   <div class="tile is-parent">
                     <div class="tile is-child box">
                       <p class="title">Gradle</p>
+                      <img src="../assets/icons/gradle.png" width="60"/>
                     </div>
                   </div>
                 </div>
               </div>
+
               <div class="tile is-vertical">
                 <div class="tile is-vertical">
                   <div class="tile is-parent">
                     <div class="tile is-child box">
                       <p class="title">Javascript</p>
+                      <i class="fab fa-js-square fa-4x"></i>
                     </div>
                   </div>
                   <div class="tile">
                     <div class="tile is-parent">
                       <div class="tile is-child box">
                         <p class="title">Angular</p>
+                        <div class="has-text-centered">
+                          <i class="fab fa-angular fa-4x"></i>
+                        </div>
+
                       </div>
                     </div>
                     <div class="tile is-parent">
                       <div class="tile is-child box">
                         <p class="title">Vue</p>
+                        <i class="fab fa-vuejs fa-4x"></i>
                       </div>
                     </div>
                   </div>
                 </div>
-
-
               </div>
+
               <div class="tile is-vertical">
-                <div class="tile is-parent">
-                  <div class="tile is-child box">
-                    <p class="title">SQL</p>
-                  </div>
-                </div>
+
                 <div class="tile is-parent">
                   <div class="tile is-child box">
                     <p class="title">Dart</p>
+                    <img src="../assets/icons/dart.png" width="60"/>
+
                   </div>
                 </div>
                 <div class="tile is-parent">
                   <div class="tile is-child box">
                     <p class="title">Flutter</p>
+                    <img src="../assets/icons/flutter.png" width="60"/>
+
                   </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="tile">
+              <div class="tile is-parent">
+                <div class="tile is-child box">
+                  <p class="title">Docker</p>
+                  <i class="fab fa-docker fa-4x"></i>
+                </div>
+              </div>
+              <div class="tile is-parent">
+                <div class="tile is-child box">
+                  <p class="title">Kubernetes</p>
+                  <img src="../assets/icons/kubernetes.png" width="60"/>
+
                 </div>
               </div>
             </div>
@@ -93,11 +124,19 @@
               <div class="tile is-parent">
                 <div class="tile is-child box">
                   <p class="title">Couchbase</p>
+                  <img src="../assets/icons/couchbase.png" width="60"/>
+                </div>
+              </div>
+              <div class="tile is-parent">
+                <div class="tile is-child box">
+                  <p class="title">SQL</p>
+                  <i class="fas fa-database fa-4x"></i>
                 </div>
               </div>
               <div class="tile is-parent">
                 <div class="tile is-child box">
                   <p class="title">Elastic Search</p>
+                  <img src="../assets/icons/elasticsearch.png" width="60"/>
                 </div>
               </div>
             </div>
@@ -106,33 +145,23 @@
               <div class="tile is-parent">
                 <div class="tile is-child box">
                   <p class="title">ActiveMQ</p>
+                  <img src="../assets/icons/activemq.jpg" width="60"/>
                 </div>
               </div>
               <div class="tile is-parent">
                 <div class="tile is-child box">
                   <p class="title">Nagios</p>
+                  <img src="../assets/icons/nagios.png" width="60"/>
                 </div>
               </div>
               <div class="tile is-parent">
                 <div class="tile is-child box">
                   <p class="title">Jenkins</p>
-                </div>
-              </div>
-            </div>
-            <div class="tile">
-              <div class="tile is-parent">
-                <div class="tile is-child box">
-                  <p class="title">Docker</p>
-                </div>
-              </div>
-              <div class="tile is-parent">
-                <div class="tile is-child box">
-                  <p class="title">Kubernetes</p>
+                  <i class="fab fa-jenkins fa-4x"></i>
                 </div>
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
