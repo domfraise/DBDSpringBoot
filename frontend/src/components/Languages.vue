2758f394250d9ff90fbc318776af6990e788ef05
FraiseD <dominic.fraise@thehutgroup.com>
2018-12-15 19:02:28 +0000

    Add more skills

--- frontend/src/components/Languages.vue
+++ frontend/src/components/Languages.vue
@@ -4,12 +4,27 @@
       <div class="container has-text-centered">
         <h2 class="is-2 title">Languages & Technologies</h2>
         <div class="tile is-ancestor">
+
           <div class="tile is-vertical is-7">
             <div class="tile is-parent">
               <div class="tile is-child box">
                 <p class="title">Java</p>
               </div>
             </div>
+
+            <div class="tile">
+              <div class="tile is-parent is-8 ">
+                <div class="tile is-child box">
+                  <p class="title">Spring</p>
+                </div>
+              </div>
+              <div class="tile is-parent">
+                <div class="tile is-child box">
+                  <p class="title">Guice</p>
+                </div>
+              </div>
+            </div>
+
             <div class="tile">
               <div class="tile is-parent ">
                 <div class="tile is-child box">
@@ -31,11 +46,26 @@
                 <p class="title">Python</p>
               </div>
             </div>
-            <div class="tile is-parent">
-              <div class="tile is-child box">
-                <p class="title">Javascript</p>
+            <div class="tile is-vertical">
+              <div class="tile is-parent">
+                <div class="tile is-child box">
+                  <p class="title">Javascript</p>
+                </div>
+              </div>
+              <div class="tile">
+                <div class="tile is-parent">
+                  <div class="tile is-child box">
+                    <p class="title">Angular</p>
+                  </div>
+                </div>
+                <div class="tile is-parent">
+                  <div class="tile is-child box">
+                    <p class="title">Vue</p>
+                  </div>
+                </div>
               </div>
             </div>
+
           </div>
 
 
