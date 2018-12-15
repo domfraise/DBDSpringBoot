f74b24b3be8717175e34b4ad8be038525730f2b0
FraiseD <dominic.fraise@thehutgroup.com>
2018-12-15 19:36:54 +0000

    Improve organisation of skills

--- frontend/src/components/Languages.vue
+++ frontend/src/components/Languages.vue
@@ -3,71 +3,107 @@
     <div class="hero-body">
       <div class="container has-text-centered">
         <h2 class="is-2 title">Languages & Technologies</h2>
+
         <div class="tile is-ancestor">
-
-          <div class="tile is-vertical is-7">
-            <div class="tile is-parent">
-              <div class="tile is-child box">
-                <p class="title">Java</p>
-              </div>
-            </div>
-
-            <div class="tile">
-              <div class="tile is-parent is-8 ">
-                <div class="tile is-child box">
-                  <p class="title">Spring</p>
-                </div>
-              </div>
-              <div class="tile is-parent">
-                <div class="tile is-child box">
-                  <p class="title">Guice</p>
-                </div>
-              </div>
-            </div>
-
-            <div class="tile">
-              <div class="tile is-parent ">
-                <div class="tile is-child box">
-                  <p class="title">Maven</p>
-                </div>
-              </div>
-              <div class="tile is-parent">
-                <div class="tile is-child box">
-                  <p class="title">Gradle</p>
-                </div>
-              </div>
-            </div>
-
-          </div>
-
           <div class="tile is-vertical">
-            <div class="tile is-parent">
-              <div class="tile is-child box">
-                <p class="title">Python</p>
+            <div class="tile">
+              <div class="tile is-vertical">
+                <div class="tile is-parent">
+                  <div class="tile is-child box">
+                    <p class="title">Java</p>
+                  </div>
+                </div>
+
+                <div class="tile">
+                  <div class="tile is-parent">
+                    <div class="tile is-child box">
+                      <p class="title">Spring</p>
+                    </div>
+                  </div>
+                  <div class="tile is-parent">
+                    <div class="tile is-child box">
+                      <p class="title">Guice</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="tile">
+                  <div class="tile is-parent ">
+                    <div class="tile is-child box">
+                      <p class="title">Maven</p>
+                    </div>
+                  </div>
+                  <div class="tile is-parent">
+                    <div class="tile is-child box">
+                      <p class="title">Gradle</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="tile is-vertical">
+                <div class="tile is-parent">
+                  <div class="tile is-child box">
+                    <p class="title">Python</p>
+                  </div>
+                </div>
+                <div class="tile is-vertical">
+                  <div class="tile is-parent">
+                    <div class="tile is-child box">
+                      <p class="title">Javascript</p>
+                    </div>
+                  </div>
+                  <div class="tile">
+                    <div class="tile is-parent">
+                      <div class="tile is-child box">
+                        <p class="title">Angular</p>
+                      </div>
+                    </div>
+                    <div class="tile is-parent">
+                      <div class="tile is-child box">
+                        <p class="title">Vue</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="tile is-parent">
+                  <div class="tile is-child box">
+                    <p class="title">Jenkins</p>
+                  </div>
+                </div>
+              </div>
+              <div class="tile is-vertical">
+                <div class="tile is-parent">
+                  <div class="tile is-child box">
+                    <p class="title">Dart</p>
+                  </div>
+                </div>
+                <div class="tile is-parent">
+                  <div class="tile is-child box">
+                    <p class="title">Flutter</p>
+                  </div>
+                </div>
+                <div class="tile is-parent">
+                  <div class="tile is-child box">
+                    <p class="title">SQL</p>
+                  </div>
+                </div>
               </div>
             </div>
-            <div class="tile is-vertical">
+
+            <div class="tile">
               <div class="tile is-parent">
                 <div class="tile is-child box">
-                  <p class="title">Javascript</p>
+                  <p class="title">Couchbase</p>
                 </div>
               </div>
-              <div class="tile">
-                <div class="tile is-parent">
-                  <div class="tile is-child box">
-                    <p class="title">Angular</p>
-                  </div>
-                </div>
-                <div class="tile is-parent">
-                  <div class="tile is-child box">
-                    <p class="title">Vue</p>
-                  </div>
+              <div class="tile is-parent">
+                <div class="tile is-child box">
+                  <p class="title">Elastic Search</p>
                 </div>
               </div>
             </div>
-
           </div>
-
 
         </div>
       </div>
