4d968ef3d48b2351355f3732ac9a7f9b62ba67e0
domfraise <domfraise@gmail.com>
2021-02-01 18:02:46 +0000

    Add some sous-chef images. Patch some vulns

--- frontend/src/components/Flutter.vue
+++ frontend/src/components/Flutter.vue
@@ -11,17 +11,30 @@
         </div>
       </div>
     </section>
-    <section class="section hero is-medium">
-      <div class="hero-head ">
+    <section class="section hero is-small">
+      <div class="hero-head">
         <div class="container has-text-centered">
-        <img src="../assets/sous-chef.png" width="100"/>
+          <img src="../assets/sous-chef.png" width="100"/>
           <h3 class="title is-3">Sous Chef - Cooking Timer</h3>
           <p>
-
+            A smart cooking timer allowing you to start multiple timers to finish at the same time. Perfect for preparing a complex roast or christmas dinner
           </p>
-
-
         </div>
+      </div>
+      <div class='hero-body'>
+          <div class='container has-text-centered'>
+              <div class='level'>
+                  <div class='container'>
+                    <img src="../assets/screenshots/main_timer.jpg" width="200"/>
+                  </div>
+                  <div class='container'>
+                    <img src="../assets/screenshots/recipe_view.jpg" width="200"/>
+                  </div>
+                  <div class='container'>
+                    <img src="../assets/screenshots/timeline_view.jpg" width="200"/>
+                  </div>
+              </div>
+          </div>
       </div>
     </section>
   </div>
